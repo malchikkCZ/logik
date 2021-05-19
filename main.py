61e1f1eb9a4246e867977b9bfdfe9026454a9151
Viktor Bem <malchikk@live.com>
2021-05-19 21:47:18 +0200

    english translation and variable refactor

--- main.py
+++ main.py
@@ -1,74 +1,88 @@
-# Na motivy hry Logik naprogramoval Malchikk.CZ (C) 2021-03-14
-
-import sys
 import os
-from random import randint
+import random
 
-def cls():
-  '''Tato funkce smaze obrazovku v ruznych OS.'''
-  os.system('cls' if os.name=='nt' else 'clear')
 
-def checkAnsw(x,y):
-  '''Tato funkce vyhodnoti odpoved a urci napovedu pro hrace.'''  
-  bkScore = 0                                   # tady se budou pocitat umistena cisla
-  whScore = 0                                   # tady se budou pocitat uhadnuta cisla
-  newY = []
-  newX = []                                     # tady se budou ukladat cisla ktere nebyly spravne umistene pro dalsi pouziti         
+LOGO = """
++--+                       +--+
+|  |                   ++  |  |
+|  |    /----\\ /----\\ +--+ |  | __
+|  |    | /\\ | | /\\ | |  | |  |/ /
+|  +--+ | \\/ | | \\/ | |  | |  |\\ \\
++-----+ \\----/ \\--+ | +--+ +--+ \\_\\
+                  /_/
+"""
 
-  for i in range(4):
-    if x[i] == y[i]:                            # nejprve se urci pocet spravne umistenych cisel
-      bkScore +=1
-    else:                                       # dvojice cisel, ktera nebyla spravne umistena, se ulozi do seznamu newX a newY
-      newX.append(x[i])
-      newY.append(y[i])
+
+def print_header():
+    print(LOGO)
+    print("You need to guess the 4-digit code composed of digits from 1 to 8. Individual digits can be repeated.")
+    print("The computer will always evaluate your guess: X = correctly placed number, O = correctly guessed number.")
+    print("You have a total of 10 attempts to guess the code.")
+    print()
+
+
+def evaluate_answer(logik, answer):
+    bk_score = 0
+    wh_score = 0
+    temp_logik = []
+    temp_answer = []
+
+    for i in range(4):
+        if logik[i] == answer[i]:
+            bk_score += 1
+        else:
+            temp_logik.append(logik[i])
+            temp_answer.append(answer[i])
   
-  for j in range(len(newX)):                    # nyni budeme pracovat pouze se zbylymi cisly
-    if newY[j] in newX:                         # nejprve pro kazde cislo v odpovedi zjistime, zda je obsazeno v generovanem cisle
-      cntX = newX.count(newY[j])
-      cntY = newY.count(newY[j])                # pokud ano, porovname kolikrat se vyskytuje v jednotlivych seznamech newX a newY
-      if cntY > cntX:
-        newY[j] = 0                             # pokud je v odpovedi vickrat nez v zadani, prepiseme ho v odpovedi na 0 a preskocime
-      else:
-        whScore +=1                             # v opacnem pripade zapocitame odpoved
-                                                # tenhle druhy cyklus zajisti, ze se nebude jedno cislo pocitat vicekrat
-  hint = bkScore*"X " + whScore*"O "
-  return hint                                   # programu posleme zpatky vysledek
+    for j in range(len(temp_logik)):
+        if temp_answer[j] in temp_logik:
+            cnt_logik = temp_logik.count(temp_answer[j])
+            cnt_answer = temp_answer.count(temp_answer[j])
+            if cnt_answer > cnt_logik:
+                temp_answer[j] = 0
+            else:
+                wh_score += 1
+
+    hint = bk_score * "X" + wh_score * "O"
+    return hint
 
 
-cls()
+def get_user_tip(attempt):
+    user_answer = input(f"Attempt nr.{attempt:2} >>> ")
+    try:
+        listed_answer = [int(x) for x in user_answer]
+        if len(listed_answer) != 4 or min(listed_answer) < 1 or max(listed_answer) > 8:
+            raise ValueError
+    except ValueError:
+        print("Your answer has to contain exactly four digits from 1 to 8.")
+        return None
+    else:
+        return listed_answer
 
-print("+--+                       +--+")
-print("|  |                   ++  |  |")
-print("|  |    /----\\ /----\\ +--+ |  | __")
-print("|  |    | /\\ | | /\\ | |  | |  |/ /")
-print("|  +--+ | \\/ | | \\/ | |  | |  |\\ \\")
-print("+-----+ \\----/ \\--+ | +--+ +--+ \\_\\")
-print("                 /_/")
-print()
 
-print("Musis uhadnout 4-mistny kod sestaveny z cislic od 1 do 8. Jednotlive cislice se mohou opakovat.")
-print("Pocitac ti vzdy napovi, jak jsi tipoval. X = spravne umistene cislo, O = spravne uhodnute cislo.")
-print("Na uhodnuti kodu mas celkem 10 pokusu. Cislice ve svem tipu oddeluj mezerou.")
-print()
+def game():
+    os.system('cls' if os.name=='nt' else 'clear')
+    print_header()
 
-logik = [randint(1, 8) for p in range(4)]                               # vygeneruje seznam 4 celych cisel v rozsahu 1 az 8 
+    logik = [random.randint(1, 8) for _ in range(4)]
 
-answer = []                                                             # definuje prazdny seznam odpovedi
-
-for i in range(1, 11):                                                  # na uhodnuti ma hrac 10 pokusu
-  while len(answer) != 4 or min(answer)<1 or max(answer)>8:             # program ceka na odpoved, dokud nebude splnovat podminku
-    answer = [int(x) for x in input(f"Pokus c.{i:2}>>> ").split()]      # program ocekava cele cisla, jinak spadne :(
+    for attempt in range(1, 11):
+        answer = get_user_tip(attempt)
+        while not answer:
+            answer = get_user_tip(attempt)
+        if answer == logik:
+            print("                  XXXX")
+            print(f"\nGreat, you won! You guessed the number: {''.join(map(str, logik))}")
+            return
+        else:
+            print(f"                  {evaluate_answer(logik, answer)}")
     
-  if answer == logik:                                                   # pokud odpoved souhlasi se zadanim, nic se nepocita a hrac vyhrava
-    print("             ", "X X X X")
-    print("\nVyhral jsi, skvele! Uhodl jsi cislo:", end=" ")
-    print(logik)
-    sys.exit()
-  else:                                                                 # v opacnem pripade se jde do podprogramu checkAnsw nahore
-    hint = checkAnsw(logik,answer)
-    print("             ", hint)                                        # a vytiskne se vyhodnoceni (napoveda)
-  
-  answer = []                                                           # musime znovu vycistit seznam odpovedi
+    print(f"\nYou missed all attempts, the correct answer was: {''.join(map(str, logik))}")
 
-print("\nVycerpal jsi vsechny pokusy, spravna odpoved byla:", end=" ")
-print(logik)                                                            # pokud hrac neuhodne v 10 pokusech, hra konci
+
+game_is_on = True
+while game_is_on:
+    game()
+    game_is_on = input("Do you want to play again? (Y/n) ").lower() != "n"
+
+print("Thank you for playing my game!")
